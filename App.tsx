c1683557e7d6a5c4bb26683b25dc4e3f7d08aa48
Arty <aatharvakawade@gmail.com>
2024-12-01 22:24:56 +0100

    creating the initial barebone calculator

--- App.tsx
+++ App.tsx
@@ -1,18 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Store from '../calc_front/src/store';
+import { Provider } from 'react-redux';
+import Screen from '../calc_front/src/components/screen';
+import NumberPad from '../calc_front/src/components/numberpad';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={Store}>
+      <View style={styles.container}>
+        <Screen/>
+        <NumberPad/>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 100,
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
