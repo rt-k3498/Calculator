ad0df69eb8c02ed564c1085fc03b4824649fb237
Arty <aatharvakawade@gmail.com>
2024-12-20 13:34:39 +0100

    adding firebase

--- ios/calcfront/main.m
+++ ios/calcfront/main.m
@@ -2,6 +2,18 @@
 
 #import "AppDelegate.h"
 
+@import UIKit;
+
+@import FirebaseCore;
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [FIRApp configure];
+  return YES;
+}
+
 int main(int argc, char * argv[]) {
   @autoreleasepool {
     return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
